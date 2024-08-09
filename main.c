f33aaa9d9b1c5c620de24415883be1c1a4b0224c
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-09 00:10:09 -0600

    Output pair of incoming arguments specified as being specific to one or the other

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+const char ws = ' ';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("either %s or %s", argv[1], argv[1 + 1]);
+
 	return 0;
 }
